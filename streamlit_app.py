a4467b6b2818a74f61595c67541839f7e13ad392
maudiwulandari <newsuser348@gmail.com>
2025-04-23 15:36:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈TUNGTUNGSHR")
 st.write(
    "HI THERE! [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("Tangkapan layar_2025-04-17-13-05-08-19_b86672daa061159f52c1a3195c773d05.jpg")
