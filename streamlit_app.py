b5af732b99cc427b5cb6d866b175babf22ecfa46
maudiwulandari <newsuser348@gmail.com>
2025-04-23 14:58:34 +0700

    Maudi

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈TUNGTUNGSHR")
 st.write(
-   HI THERE! "[docs.streamlit.io](https://docs.streamlit.io/)."
+   "HI THERE! [docs.streamlit.io](https://docs.streamlit.io/)."
 )
