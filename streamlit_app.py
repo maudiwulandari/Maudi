08f4289d990f4eb2eabc16b728d9d53e2faa8d71
maudiwulandari <newsuser348@gmail.com>
2025-05-21 21:18:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("˚༄˖°.🍂.ೃ࿔*:･")
 st.write(
-   "HI THERE! [docs.streamlit.io](https://docs.streamlit.io/)."
+   "  ࣪ ˖ ࣪ ᨰꫀᥣᥴ᥆ꩇꫀ ! ᰔ ִ ׄ [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.image("Screenshot_2025-04-17-13-05-08-19_b86672daa061159f52c1a3195c773d05.jpg")
 st.title("Aplikasi Sederhana")
